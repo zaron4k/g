79409439a0f31a577281abc8e5596461f8161f73
zaron4k <76218026+zaron4k@users.noreply.github.com>
2025-02-21 12:45:20 +0200

    Add files via upload

---
+++ ConsoleApp2/Edition.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Edition
+    {
+        private const int V = 0;
+        private string title;
+        private string author;
+        private int year;
+
+        public string Name
+        {
+            get { return (title != "") ? title : "Неизвестно"; }
+            set => title = value;
+        }
+          
+            
+
+        public string Author
+        {
+            get { return (author != "") ? author : "Неизвестно"; }
+            set => author = value;
+        }
+
+        public int Year
+        {
+            get => year;
+            set
+            {
+                if (value < 1990)
+                    year = 1990;
+                else if (value > 2024)
+                    year = 2024;
+                else year = value;
+                if (value < 0)
+                    throw new ArgumentException("Неизвестный");
+
+            }
+        }
+
+     public Edition(string name, string Author, int year)
+        {
+            Name = name;
+            Author = author;
+            Year = year;
+        }
+
+        public Edition()
+        {
+        }
+        public Edition(string name, string author, double year)
+        {
+            this.Name = name;
+            this.Author = author;
+            this.Year = (int)year;
+        }
+        public override string ToString()
+        {
+            return $"Имя: {Name}\nАвтор: {Author}\nГод: {Year} ";
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Edition Ed = new Edition();
+            Ed.Name = "War and world";
+            Ed.Author = "Rom";
+            Ed.Year = 2002;
+            Console.WriteLine($"{Ed.ToString()}");
+            Console.WriteLine();
+
+            Edition Ed2 = new Edition("Пушкин", "Чайковский", 88);
+            Console.WriteLine($"{Ed2.ToString()}");
+            Console.WriteLine();
+
+            Console.Write("Название: ");
+            string name = Console.ReadLine();
+            if (string.IsNullOrEmpty(name))
+            {
+                name = "Неизвестный";
+            }
+
+            Console.Write("Имя: ");
+            string author = Console.ReadLine();
+            if (string.IsNullOrEmpty(author))
+            {
+                author = "Неизвестный";
+            }
+
+            Console.Write("Год: ");
+            string yearInput = Console.ReadLine();
+            double year;
+            if (string.IsNullOrEmpty(yearInput) || !double.TryParse(yearInput, out year))
+            {
+                year = 0; 
+            }
+
+            Edition Ed3 = new Edition(name, author, year);
+            Console.WriteLine($"{Ed3.ToString()}");
+            Console.ReadLine();
+
+        }
+    }
+}
